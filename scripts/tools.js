3383cd1ab038c0269d47f4b809744f9df26be5b5
Mathilde BOLTENHAGEN <mathilde.boltenhagen@outlook.com>
2020-03-22 16:31:09 +0100

    enregistrer et charger ok

--- scripts/tools.js
+++ scripts/tools.js
@@ -30,7 +30,37 @@ function exportFile(){
 }
 
 function loadFile(){
-    
-
+    document.getElementById("popupCharger").style.display="block";
 }
 
+function uploadFile(files){
+    var reader = new FileReader();
+    reader.readAsText(files[0]);
+    reader.onload = loadHandler;
+
+    function loadHandler(event) {
+        mydata=[];
+        lines=event.target.result.split("\n");
+        columns=lines[0].split("\t");
+        mydata["columns"]=columns;
+        for (var i=1; i<lines.length;i++){
+            var smallDict={};
+            for (c in columns){
+                // ici ya un pb si une case est vide... ne pas faire de cases vides?
+                smallDict[columns[c]]=lines[i].split("\t")[c];
+            }
+            mydata.push(smallDict);
+        }
+       const p2 = new Promise(function(resolve, reject) {
+            // columns
+            dataProcess(mydata);
+
+           });
+       // create boxes and dots
+       p2.then(updatesBoxesAndMapDueToFilter());
+    }
+}
+
+function endLoad(){
+    document.getElementById("popupCharger").style.display="none";
+}
